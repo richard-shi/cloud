1e17b28f3f1d5f8131bc5929330519df21caca35
Richard Shi <mail@richardshi.com>
2018-07-29 22:05:29 -0500

    I dont know what I was working on but I'm going to push

--- cloud.lua
+++ cloud.lua
@@ -6,19 +6,19 @@ local cloud = {}
 -- Constants
 local TWO_PI = 2*math.pi
 local DEFAULT_RADIUS = 100
-local DEFAULT_COLOR = {r = 255, g = 255, b = 255}
-local DEFAULT_CIRCLE_COUNT = 30
+local DEFAULT_COLOR = {r = 255, g = 255, b = 255, a = 0.15}
+local DEFAULT_CIRCLE_COUNT = 25
 
 -- The radius of circles used are this times the entire clouds radius
-local CIRCLE_RADIUS_RATIO = 0.4
+local CIRCLE_RADIUS_RATIO = 0.6
 
 -- Linear radial gradient functions
-function cloud.radialGradient(radius, r, g ,b)
+function cloud.radialGradient(radius, r, g, b, a)
   local data = love.image.newImageData(radius * 2, radius * 2)
 
   data:mapPixel(function(x, y)
       local dist = cloud.distance(radius, radius, x, y)
-      return r, g, b, (dist <= radius and cloud.scale(dist, 0, radius, 255, 0) or 0)
+      return r, g, b, (dist <= radius and cloud.scale(dist, 0, radius, 255, 0) * a or 0)
   end)
 
   return love.graphics.newImage(data)
@@ -50,7 +50,7 @@ function cloud.getCloud(radius, color, circleCount)
   love.graphics.setBlendMode("alpha")
 
   -- Generate circle
-  circleImage = cloud.radialGradient(circleRadius, color.r, color.g, color.b)
+  circleImage = cloud.radialGradient(circleRadius, color.r, color.g, color.b, color.a)
   love.graphics.setColor(color.r, color.g, color.b)
 
   -- Draw circles within cloud area
